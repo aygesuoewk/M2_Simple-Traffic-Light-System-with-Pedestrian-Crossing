62ba42f4f1d4a4627c08a19494d2fa091b84bd84
Stephen <js3857@srmist.edu.in>
2022-04-21 13:52:35 +0530

    Files added

---
+++ 3_Implementation/inc/traffic.h
@@ -0,0 +1,14 @@
+#ifndef TRAFFIC_SIGNAL_SYSTEM_H_
+#define TRAFFIC-SIGNAL-SYSTEM_H
+
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB1)   /**< LED Pin number  */
+
+#include <avr/io.h>
+
+#endif /* TRAFFIC_SIGNAL_SYSTEM_H_
--- 3_Implementation/src/traffic.c
+++ 3_Implementation/src/traffic.c
@@ -1,7 +1,18 @@
+/**
+ * @file traffic.c
+ * @author Stephen J
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #define F_CPU 16000000L
 
 #include <avr/io.h>          
 #include <util/delay.h>
+
 
 
 
